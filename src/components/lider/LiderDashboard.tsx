34f5e1dde6a8adb9bb472305cc0bf52656484b44
Cesar Sarchioni <csarchioni@ater.gob.ar>
2026-01-26 17:47:55 -0300

    feat: import CumpleanosModal in LiderDashboard for birthday management

--- src/components/lider/LiderDashboard.tsx
+++ src/components/lider/LiderDashboard.tsx
@@ -7,7 +7,9 @@ import { PendientesModal } from './PendientesModal';
 import { HistorialAsistenciasModal } from './HistorialAsistenciasModal';
 import { MaterialesModal } from '../common/MaterialesModal';
 import { NoticiasModal } from '../common/NoticiasModal';
+
 import { DonacionesModal } from '../common/DonacionesModal';
+import { CumpleanosModal } from '../common/CumpleanosModal';
 
 import { Users, UserPlus, Calendar, Crown, Star, Trash2, Edit, CheckCircle2, XCircle, Bell, FileText, Newspaper, Heart, History } from 'lucide-react';
 
